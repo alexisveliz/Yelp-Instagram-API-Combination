c853c951ce2cf1cc6448c5336f0d8b686aa0cd9a
haleyanderson4 <haleyanderson4@comcast.net>
2019-11-04 09:48:36 -0800

    Updated supported locations

--- Connection.py
+++ Connection.py
@@ -2,7 +2,7 @@
 
 import os
 
-location = input("Where are you looking to eat?\nCurrently Supported Locations: San Francisco, Chicago\nLocation: ")
+location = input("Where are you looking to eat?\nCurrently Supported Locations: Los Angeles, San Francisco, Seattle\nLocation: ")
 term = input("What kind of food are you looking for?\nExamples: bars, dinner, Thai, etc\nResponse: ")
 
 #os.system(python YelpAPI.py --term= %s --location= %s)
