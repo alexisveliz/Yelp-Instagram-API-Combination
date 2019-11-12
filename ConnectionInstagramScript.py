cd02d0f6ec2c35fc141bbc3986ca9bcf3d156de4
Alejandro Ferrer-Peasley <36723243+alexferrer17@users.noreply.github.com>
2019-11-11 17:39:40 -0800

    Create ConnectionInstagramScript.py

---
+++ ConnectionInstagramScript.py
@@ -0,0 +1,10 @@
+#script that gets keywords from yelp, passes and converts it into search  location command for the api
+ 
+ import os
+ 
+ os.system("pip3 install instaloader")
+ 
+ os.system("instaloader restaurantvibess")
+ 
+ 
+ 
