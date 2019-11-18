9a6c3dc2e617287666764aef5a2cc48d35f6832b
Aimee Bowen <bowen126@mail.chapman.edu>
2019-11-18 12:47:11 -0800

    started to workout website and UI

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from bottle import route, post, request, static_file
+from bottle import route, template, request, static_file
+
+@route('/style.css')
+def server_static(filepath="style.css"):
+    return static_file(filepath, root='')
 
 @route('/')
-def server_static(filepath="index.html"):
-    return static_file(filepath, root="")
+@route('/search')
+def login():
+    search_form = '''
+        <form action="/results" method="post">
+            Term: <input name="term" type="text" />
+            Location: <input name="location" type="text" />
+            <input value="Search" type="submit" />
+        </form>
+    '''
+    return template('base', code=search_form)
 
-@post('/results')
-def process():
+@route('/results', method='POST')
+def do_login():
     term = request.forms.get('term')
     location = request.forms.get('location')
-    return "You searched for {0} in {1}".format(term, location)
+    results = '<p>You searched for {0} in {1}</p>'.format(term,location)
+    return template('base', code=results)
---
+++ views/base.tpl
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Title</title>
+<head>
+</head>
+<body>
+    <div class="container">
+        %if True:
+            {{!code}}
+        %end
+    </div>
+</body>
+</html>
