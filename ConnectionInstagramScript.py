dd8b08431832e41f78d7e0cf6163f9691d66927f
Alejandro Ferrer <ferrerpeasley@mail.chapman.edu>
2019-11-26 12:00:48 -0700

    error handling and pip install all the packages

--- ConnectionInstagramScript.py
+++ ConnectionInstagramScript.py
@@ -1,14 +1,26 @@
-#script that gets keywords from yelp, passes and converts it into search  location command for the api
-# Implementation Alejandro, Haley Anderson and Elise May
+
+class LoginError(Exception):
+   """No need to login in account"""
+   pass
 
 import os
 
-username = "restaurantvibess"
-os.system("rm -rf pictures")
+while True:
+  try:
+    os.system("pip3 install urllib3")
+    os.system("pip3 install bottle")
+    username = "restaurantvibess"
+    os.system("rm -rf pictures")
 
-os.system("pip3 install instaloader")
-string = "instaloader %s"%(username)
-os.system(string)
+    os.system("pip3 install instaloader")
+    string = "instaloader %s"%(username)
+    os.system(string)
 
-string = "mv %s pictures"%(username)
-os.system(string)
+    string = "mv %s pictures"%(username)
+    os.system(string)
+    raise LoginError
+    break
+  except LoginError:
+    print("Downloaded completed, No need for login")
+    print("")
+    break
