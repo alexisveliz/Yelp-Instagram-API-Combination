916089aa956cfe4c34fe8b20d1fa92fe226bac20
Haley Anderson <haleyanderson4@comcast.net>
2019-12-02 16:58:51 -0800

    Add Main method to script

--- Connection.py
+++ Connection.py
@@ -7,25 +7,26 @@ import requests
 location = input("Where are you looking to eat?\nCurrently Supported Locations: Los Angeles, San Francisco, Seattle\nLocation: ")
 term = input("What kind of food are you looking for?\nExamples: bars, dinner, Thai, etc\nResponse: ")
 
-SEARCH_LIMIT = 5
+def YelpSearch(location, term):
+    SEARCH_LIMIT = 5
 
-parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-parser.add_argument('-q', '--term', dest='term', default=term,
-                    type=str, help='Search term (default: %(default)s)')
-parser.add_argument('-l', '--location', dest='location',
-                    default=location, type=str,
-                    help='Search location (default: %(default)s)')
+    parser.add_argument('-q', '--term', dest='term', default=term,
+                        type=str, help='Search term (default: %(default)s)')
+    parser.add_argument('-l', '--location', dest='location',
+                        default=location, type=str,
+                        help='Search location (default: %(default)s)')
 
-input_values = parser.parse_args()
+    input_values = parser.parse_args()
 
-try:
-    YelpAPI.query_api(input_values.term, input_values.location)
-except HTTPError as error:
-    sys.exit(
-        'Encountered HTTP error {0} on {1}:\n {2}\nAbort program.'.format(
-            error.code,
-            error.url,
-            error.read(),
+    try:
+        YelpAPI.query_api(input_values.term, input_values.location)
+    except HTTPError as error:
+        sys.exit(
+            'Encountered HTTP error {0} on {1}:\n {2}\nAbort program.'.format(
+                error.code,
+                error.url,
+                error.read(),
+            )
         )
-    )
